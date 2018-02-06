73339730463d4f5d55052699ed0423b495da8e6d
rajkumar1962 <35728415+rajkumar1962@users.noreply.github.com>
2018-02-06 16:15:39 +0530

    Create alphabetorno.c

---
+++ alphabetorno.c
@@ -0,0 +1,24 @@
+
+#include <stdio.h>
+
+int main()
+{
+    char ch;
+ printf("Enter any alphabet ");
+    scanf("%c", &ch);
+
+    switch(ch)
+    {
+        case 1: 
+            printf("enter the value");
+            break;
+        case 2: 
+            printf("enter the value");
+            break;
+      
+        default: 
+            printf("alphabet");
+    }
+
+    return 0;
+}
